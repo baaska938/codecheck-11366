c80e2594c9360fe13b17aea276a3ba31e3e111dd
code-project <dev@code-check.io>
2017-05-15 08:51:36 +0000

    Initial commit

---
+++ java/fw/src/main/java/codecheck/App.java
@@ -0,0 +1,11 @@
+package codecheck;
+
+public class App {
+	public static void main(String[] args) {
+		for (int i = 0, l = args.length; i < l; i++) {
+			System.out.println(args[i]);
+        
+		}
+        System.out.println("Baaska");
+	}
+}
